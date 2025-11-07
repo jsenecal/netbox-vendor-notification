f690deb81843940ab69808cf27f65d632e500622
Jonathan Senecal <jonathan.senecal@metrooptic.com>
2025-11-07 20:10:13 +0000

    chore: reset version to 0.1.0 for new project
    Update version number to 0.1.0 as this represents a fresh start for the netbox-vendor-notification project.

--- vendor_notification/__init__.py
+++ vendor_notification/__init__.py
@@ -2,15 +2,12 @@
 
 __author__ = """Jonathan Senecal"""
 __email__ = "contact@jonathansenecal.com"
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 
 
 from netbox.plugins import PluginConfig
 
 from .constants import DEFAULT_ALLOWED_CONTENT_TYPES
-
-# Template extensions will be populated dynamically in ready()
-template_extensions = []
 
 
 class VendorNotificationConfig(PluginConfig):
@@ -34,12 +31,6 @@ class VendorNotificationConfig(PluginConfig):
     def ready(self):
         super().ready()
         from . import widgets  # noqa: F401
-
-        # Register dynamic template extensions
-        from .template_content import create_event_history_extensions
-
-        global template_extensions
-        template_extensions.extend(create_event_history_extensions())
 
 
 config = VendorNotificationConfig
